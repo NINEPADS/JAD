02d401f1b910bbc75468a00de4ea24d8128b0afe
NINEPADS <121380093+NINEPADS@users.noreply.github.com>
2025-01-21 17:10:35 +0800

    Update SearchEngine.java

--- hw/homework3/src/SearchEngine.java
+++ hw/homework3/src/SearchEngine.java
@@ -1,11 +1,3 @@
-/*
- * @Author: NINEPADS 1578066861@qq.com
- * @Date: 2024-11-07 20:27:18
- * @LastEditors: NINEPADS 1578066861@qq.com
- * @LastEditTime: 2024-11-09 14:28:24
- * @FilePath: \homework3\src\searchEngine.java
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import java.util.Scanner;
 
 /**
